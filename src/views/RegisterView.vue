4944f094f2ce537b9b6f24e61f21675b32ab207a
FPacherres <fpacherres.tech@gmail.com>
2022-11-26 23:46:49 -0500

    create register view

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="register">
+    <h1>Register</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'RegisterView',
+  components: {}
+}
+</script>
